3fd206d747739535203829aa43e3926a20a969b6
Mohamed Ehab Ali <mohamed.ehab31450@gmail.com>
2022-06-24 20:19:15 +0200

    Domain Models Added

--- src/main/java/com/example/articlewebapp/domain/User.java
+++ src/main/java/com/example/articlewebapp/domain/User.java
@@ -91,17 +91,17 @@ public class User {
     )
     private Set<Authority> authorities = new HashSet<>();
 
-    @ManyToMany(mappedBy = "following", cascade = CascadeType.ALL)
-    @JoinTable(name="UserRel",
-            joinColumns={@JoinColumn(name="ParentId")},
-            inverseJoinColumns={@JoinColumn(name="UserId")})
-    private Set<User> followers = new HashSet<User>();
-
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name="UserRel",
-            joinColumns={@JoinColumn(name="UserId")},
-            inverseJoinColumns={@JoinColumn(name="ParentId")})
-    private Set<User> following = new HashSet<User>();
+//    @ManyToMany(mappedBy = "following", cascade = CascadeType.ALL)
+//    @JoinTable(name="UserRel",
+//            joinColumns={@JoinColumn(name="ParentId")},
+//            inverseJoinColumns={@JoinColumn(name="UserId")})
+//    private Set<User> followers = new HashSet<User>();
+//
+//    @ManyToMany(cascade = CascadeType.ALL)
+//    @JoinTable(name="UserRel",
+//            joinColumns={@JoinColumn(name="UserId")},
+//            inverseJoinColumns={@JoinColumn(name="ParentId")})
+//    private Set<User> following = new HashSet<User>();
 
 
 
