c7a0e87932ebd28d3a1d904e472ef73e5fc21bdd
Youssef Agagg <youssefagagg@gmail.com>
2022-06-26 01:19:55 +0200

    test mapping dto to entity

---
+++ src/test/java/com/example/articlewebapp/service/dto/mapper/ArticleLikesDisLikesMapperTest.java
@@ -0,0 +1,12 @@
+package com.example.articlewebapp.service.dto.mapper;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleLikesDisLikesMapperTest {
+    // TODO write test cases for ArticleLikesDisLikesMapper
+    @BeforeEach
+    void setUp() {
+    }
+}
---
+++ src/test/java/com/example/articlewebapp/service/dto/mapper/ArticleMapperTest.java
@@ -0,0 +1,13 @@
+package com.example.articlewebapp.service.dto.mapper;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleMapperTest {
+// TODO write test cases for ArticleMapper
+    @BeforeEach
+    void setUp() {
+
+    }
+}
