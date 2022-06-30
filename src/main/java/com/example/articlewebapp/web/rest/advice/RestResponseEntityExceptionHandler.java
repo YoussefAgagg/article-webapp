ad4558b381fd47436b4a741f8c62ad22b76fd365
Youssef Agagg <youssefagagg@gmail.com>
2022-06-30 16:09:51 +0200

    handle custom exception throws

--- src/main/java/com/example/articlewebapp/web/rest/advice/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/articlewebapp/web/rest/advice/RestResponseEntityExceptionHandler.java
@@ -1,11 +1,10 @@
 package com.example.articlewebapp.web.rest.advice;
 
-import com.example.articlewebapp.exception.EmailAlreadyUsedException;
-import com.example.articlewebapp.exception.InvalidPasswordException;
-import com.example.articlewebapp.exception.UsernameAlreadyUsedException;
+import com.example.articlewebapp.exception.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -52,5 +51,25 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
 
     return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
 
+  }
+  @ExceptionHandler(ChangeUsernameException.class)
+  public ResponseEntity<Object> changeUserNameException(Exception ex) {
+
+    return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+  }
+  @ExceptionHandler(ResourceDoesntExistException.class)
+  public ResponseEntity<Object> accountDoesntExistException(Exception ex) {
+
+    return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.NOT_FOUND);
+  }
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<Object> accessDeniedException(Exception ex) {
+
+    return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.FORBIDDEN);
+  }
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<Object> badRequestExceptionHandler(Exception ex) {
+
+    return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.BAD_REQUEST);
   }
 }
